d2ea5dcb9763abc54f1e5128cd734fbbc1636402
Odera Igwe <oderagames@gmail.com>
2020-11-03 15:45:40 +0400

    Assets

---
+++ Assets/Resources/Srikar/Scripts/Character/AnimationMaster.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationMaster : MonoBehaviour
+{
+    public static AnimationMaster instance = null;
+
+    [SerializeField] uint framesPerSecond;
+    [SerializeField] uint resetValue = 1;
+
+    [SerializeField] float freezeFrameDuration = 0.5f;
+
+    [SerializeField] float gravity = 1f;
+
+    float universalTimer = 0;
+
+    private void Awake()
+    {
+        if (instance != null)
+            Destroy(gameObject);
+        else
+            instance = this;
+    }
+
+    public float AnimFrames
+    {
+        get { return framesPerSecond; }
+    }
+
+    public float Freeze
+    {
+        get { return freezeFrameDuration; }
+    }
+
+    public float Gravity
+    {
+        get { return gravity; }
+    }
+}
---
+++ Assets/Resources/Srikar/Scripts/UIInputHistory.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIInputHistory : MonoBehaviour
+{
+    [SerializeField] UIInputKeys prefab;
+    [SerializeField] int inputLimit = 20;
+
+    UIInputKeys current;
+
+    List<UIInputKeys> inputList;
+
+    bool active;
+
+    bool currentNull;
+
+    private void Start()
+    {
+        active = true;
+        inputList = new List<UIInputKeys>();
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public void AddInput(InputKey key)
+    {
+        if (active)
+        {
+            current = Instantiate(prefab, this.transform);
+            current.Assign(key);
+            inputList.Insert(0, current);
+            current.transform.SetSiblingIndex(0);
+        }
+
+        if (inputList.Count > inputLimit)
+        {
+            var temp = inputList[inputList.Count - 1];
+            inputList.Remove(temp);
+            Destroy(temp.gameObject);
+        }
+    }
+
+    public void UpdateCurrentFrames(int frameCount)
+    {
+        if (active && current != null)
+            current.FrameUpdate(frameCount);
+    }
+}
