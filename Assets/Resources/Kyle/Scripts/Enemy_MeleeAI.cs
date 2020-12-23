f3974611c6c79adc7376e93c20a164a9cadf3582
LoveVector <superprunejuice@gmail.com>
2020-12-23 05:39:02 +0400

    Attempted to AI, doesn't work properly, only moves towards you

---
+++ Assets/Resources/Kyle/Scripts/Enemy_MeleeAI.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_MeleeAI : MonoBehaviour
+{
+    public Transform rayCast;
+    public float moveSpeed;
+    public float raycastLength;
+    public float stalkingDistance;
+    public float maxStalkTimer;
+    public float maxCooldownTime;
+    public Vector3 offset;
+    float attackDistance;
+
+    LayerMask raycastMask;
+    RaycastHit2D hit;
+    GameObject target;
+    Animator animCont;
+    [SerializeField] float distance;
+    [SerializeField] float stalkTimer;
+    [SerializeField] float currentCooldownTime;
+    [SerializeField] bool isAttacking;
+    [SerializeField] bool inRange;
+    [SerializeField] bool isCooldown;
+    [SerializeField] private bool facingRight = true;
+    Vector2 playerFlip;
+
+    private Rigidbody2D rb2D;
+
+
+    private void Awake()
+    {
+        animCont = GetComponent<Animator>();
+        target = GameObject.FindWithTag("Player");
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {   
+        distance = Vector2.Distance(transform.position, target.transform.position);
+        Move();
+        Flip();
+
+    }
+
+    void Flip()
+    {
+        playerFlip = transform.position - target.transform.position;
+        if (transform.position.x > target.transform.position.x && facingRight)
+        {
+            facingRight = !facingRight;
+            //transform.position += Vector3.right * 20f;
+            transform.Rotate(0, 180, 0);
+        }
+        else if (transform.position.x < target.transform.position.x && !facingRight)
+        {
+            facingRight = !facingRight;
+            //transform.position += Vector3.right * -20f;
+            transform.Rotate(0, 180, 0);
+        }
+    }
+
+    void Move()
+    {
+        /*Vector2 newPosition = target.transform.position;
+        newPosition.y = transform.position.y;
+        newPosition.x = transform.position.x;*/
+
+        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, Time.deltaTime * moveSpeed);
+        rb2D.MovePosition(transform.position);
+        animCont.SetBool("isWalking", true);
+        //Stalking();
+    }
+
+    void Attack()
+    {
+
+    }
+
+    void Stalking()
+    {
+        stalkTimer -= Time.deltaTime;
+        if (!animCont.GetCurrentAnimatorStateInfo(0).IsName("Enemy1_Attack"))
+        {
+            Vector2 newPosition = new Vector2(target.transform.position.x, transform.position.y);
+            Vector3 stalkTargetPos = newPosition;
+            stalkTargetPos.y = transform.position.y;
+            stalkTargetPos.z = transform.position.z;
+            transform.position = Vector2.MoveTowards(transform.position, stalkTargetPos, moveSpeed * Time.deltaTime);
+
+            if (distance <= stalkingDistance)
+            {
+                stalkTimer = maxStalkTimer;
+                if (stalkTimer < 0)
+                {
+
+                }
+            }
+        }
+    }
+}
